d54908a261a5d92ba3a3431b0f919b645e7c43be
David Chmarzynski <david.chm00@gmail.com>
2021-02-16 08:25:55 +0100

    Starter Nextjs

---
+++ client/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.scss';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>David Chmarzynski</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hello</h1>
+    </div>
+  )
+}
