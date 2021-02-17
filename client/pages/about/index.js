2a29f9e96bd78c7e946791e35524364a90b3e6b2
David Chmarzynski <david.chm00@gmail.com>
2021-02-17 11:57:22 +0100

    Responsive About component

--- client/pages/about/index.js
+++ client/pages/about/index.js
@@ -18,29 +18,31 @@ export default function About() {
       <div className={styles.wrapper}>
         <div className={styles.backgroundRed}></div>
         <h2 className={styles.title}>Développeur web <br/>Fullstack</h2>
-        <div className={styles.bio}>
-          <h3 className={styles.h3}>Bio</h3>
-          <p className={styles.p}>
-            Je suis développeur web, spécialisé dans les technologies liées au Javascript
-            alliants efficacité, productivité, et puissance. <br/>
-            Basé à <span className={styles.city}>Avignon</span>, je suis #OpenToWork.
-          </p>
-        </div>
-        <div className={styles.stack}>
-          <h3 className={styles.h3}>Stack Technique</h3>
-          <div className={styles.stackList}>
-            <ul className={styles.languages}>
-              <h4 className={styles.h4}>Langages :</h4>
-              {languages.map(el => (
-                <li>{el.name}</li>
-              ))}
-            </ul>
-            <ul className={styles.frameworks}>
-              <h4 className={styles.h4}>Frameworks :</h4>
-              {frameworks.map(el => (
-                <li>{el.name}</li>
-              ))}
-            </ul>
+        <div className={styles.skills}>
+          <div className={styles.bio}>
+            <h3 className={styles.h3}>Bio</h3>
+            <p className={styles.p}>
+              Je suis développeur web, spécialisé dans les technologies liées au Javascript
+              alliants efficacité, productivité, et puissance. <br/>
+              Basé à <span className={styles.city}>Avignon</span>, je suis #OpenToWork.
+            </p>
+          </div>
+          <div className={styles.stack}>
+            <h3 className={styles.h3}>Stack Technique</h3>
+            <div className={styles.stackList}>
+              <ul className={styles.languages}>
+                <h4 className={styles.h4}>Langages :</h4>
+                {languages.map(el => (
+                  <li>{el.name}</li>
+                ))}
+              </ul>
+              <ul className={styles.frameworks}>
+                <h4 className={styles.h4}>Frameworks :</h4>
+                {frameworks.map(el => (
+                  <li>{el.name}</li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
